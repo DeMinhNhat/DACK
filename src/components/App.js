86a138e02f88b7f09703388df8031cd980ca412e
docaotri123 <docaotri211997@gmail.com>
2018-12-01 23:00:37 +0700

    [1512602] init routes login,logout

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+import indexRoutes from '../routes/index';
+
+var hist = createBrowserHistory();
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        hello :)
-      </div>
+    return (<div>
+      <Router history={hist}>
+        <Switch>
+          {indexRoutes.map((prop, key) => {
+            return <Route path={prop.path} key={key} component={prop.components} />
+          })}
+        </Switch>
+      </Router>
+    </div>
     );
   }
 }
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import Login from '../components/login/login';
+import Logout from '../components/logout/logout';
+
+const indexRoutes = [
+    {path: '/login', components: Login},
+    {path: '/logout', components: Logout},
+]
+
+export default indexRoutes;

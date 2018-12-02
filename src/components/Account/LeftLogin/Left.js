debb84e7fee1d40ed6e01794b2eab63f6df23c90
docaotri123 <docaotri211997@gmail.com>
2018-12-02 14:12:49 +0700

    [1512602] content component LeftLogin of

--- src/components/Account/LeftLogin/Left.js
+++ src/components/Account/LeftLogin/Left.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
+const ContentLeft = () =>{
+    return (
+        <div className="contentLeft">
+            <div className="inline" >
+                <p><i class="fas fa-search"></i> <span >Follow your interests</span>.</p>
+                <p><i class="fas fa-users"></i> <span>Hear what people are talking about</span>.</p>
+                <p><i class="far fa-comment"></i> <span>Join the conversation</span>.</p>
+            </div>
+        </div>
+    )
+}
+
 const LeftLogin = () =>{
     return (
         <div className="leftBar">
-            Left
+            <ContentLeft />
         </div>
     )
 }

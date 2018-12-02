f3590c80c562772b1e1763217af443c8ffd976a7
docaotri123 <docaotri211997@gmail.com>
2018-12-02 16:00:40 +0700

    [1512602] Basic component Login will update in future :v

--- src/components/Account/Login.js
+++ src/components/Account/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import LeftLogin  from'./LeftLogin/Left';
 
@@ -19,7 +21,48 @@ class Login extends Component{
                     <LeftLogin />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    Right
+                    <div className="rightLogin">
+                        <div className="blockFromLogin">
+                            <form>
+                                <div className="formLogin">
+                                    <div className="inline">
+                                        <TextField
+                                            id="standard-dense"
+                                            label="username"
+                                            margin="dense"
+                                        />
+                                    </div>
+                                    <div className="inline2">
+                                        <div>
+                                            <TextField
+                                                id="standard-dense"
+                                                label="Password"
+                                                type="password"
+                                                margin="normal"
+                                            />
+                                        </div>
+                                        <a href="#"> forgot password?</a>
+                                    </div>
+                                    <div className="inline3">
+                                        <Button variant="outlined" color="primary">
+                                            Log In
+                                </Button>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                        <div className="blockContent">
+                            <i class="fab fa-twitter"></i>
+                            <p>See what’s happening in the world right now </p>
+                        </div>
+                        <div className="clockSignUp">
+                            <p>Join Twitter today.</p>
+                            <Button variant="contained" color="primary">
+                                Sign Up
+                            </Button>
+                        </div>
+                    </div>
                 </Grid>
             </Grid>
         )

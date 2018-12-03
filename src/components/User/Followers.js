6ccad612784e780975090f1d2ca9d0b173a13dfc
docaotri123 <docaotri211997@gmail.com>
2018-12-04 00:41:07 +0700

    [1512602] followers will update

--- src/components/User/Followers.js
+++ src/components/User/Followers.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 
+import '../../utils/following.css'
+
+const ProfileCard = (props)=>{
+    return (
+        <div class="card">
+            <img src={props.data.img} alt="John" />
+                <h1>{props.data.name}</h1>
+                <p class="title">CEO & Founder, Example</p>
+                <p>{props.data.university}</p>
+                <div className="icon">
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </div>
+                <p><button>Followers</button></p>
+        </div>
+
+    );
+}
+        
 export default class Followers extends Component{
     constructor(props){
         super(props);
@@ -7,10 +29,31 @@ export default class Followers extends Component{
     }
 
     render(){
+        const element =  people.map((val,index) => <Grid item xs={12} sm={4} key={index}> <ProfileCard data={val}/> </Grid>);
+        console.log(element);
         return(
-            <div>
-                Followers
-            </div>
+            <Grid container spacing={5}>
+                {element}
+            </Grid>
         )
     }
 }
+
+// hardcode to test :V
+const people = [
+    {
+        img:'http://img.bongdanet.vn/application/admin/image/18.12.2017/Kaka_-_giai_nghe.jpg',
+        name:'Kaka',
+        university:'KHTN'
+    },
+    {
+        img:'http://img.bongdanet.vn/application/admin/image/18.12.2017/Kaka_-_giai_nghe.jpg',
+        name:'Messi',
+        university:'BK'
+    },
+    {
+        img:'http://img.bongdanet.vn/application/admin/image/18.12.2017/Kaka_-_giai_nghe.jpg',
+        name:'CR7',
+        university:'KHTN'
+    }
+];
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ import Login from '../components/Account/Login';
 import SignUp from '../components/Account/SignUp';
 import Home from '../components/Home/Home';
 import User from '../components/User/User';
+import Followers from '../components/User/Followers'
 
 const indexRoutes = [
+    {path: '/followers', components: Followers},
     {path: '/login', components: Login},
     {path: '/signup', components: SignUp},
     {path: '/user', components: User},
